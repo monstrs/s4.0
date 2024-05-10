3f10c9dc6a8ae9b8c71eeaf397af7c77bdfe12f8
Andrey Linko <AndreyLinko@gmail.com>
2024-05-10 13:11:23 +0300

    feat: change default redirect url

--- applications/app/entrypoints/renderer/src/app/page.tsx
+++ applications/app/entrypoints/renderer/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation.js'
 
 export default (): void => {
-  redirect('/sports/figure-skating/organizations')
+  redirect('/sports/figure-skating/organization/new')
 }
